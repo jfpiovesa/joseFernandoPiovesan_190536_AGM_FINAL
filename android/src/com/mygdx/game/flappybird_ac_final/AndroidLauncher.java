cb6f6a14d4bd23bacf4d41e9069c0cb26aa39c61
jfpiovesa <78732629+jfpiovesa@users.noreply.github.com>
2021-06-07 23:15:19 -0300

    Initial commit

---
+++ android/src/com/mygdx/game/flappybird_ac_final/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.game.flappybird_ac_final;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.game.flappybird_ac_final.Jogo;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Jogo(), config);
+	}
+}
